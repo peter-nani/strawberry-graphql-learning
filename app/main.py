9a9d8574029f0154a841f21be544dc19f04a4638
prasanna-kumar <prasanna-kumar@gmail.com>
2026-07-22 21:33:59 +0530

    version:2 of query and mutations

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-
+import uvicorn
+import strawberry
 from strawberry.fastapi import GraphQLRouter
-
 from schema import schema
-
 
 app = FastAPI()
 
 graphql_app = GraphQLRouter(schema)
 
 app.include_router(graphql_app, prefix="/graphql")
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- app/schema.py
+++ app/schema.py
@@ -1,60 +1,20 @@
 import strawberry
-from strawberry.schema.config import StrawberryConfig
 
 @strawberry.type
 class Student:
     name:str
-    id:int 
     age:int
-# -----------------------------
-# Fake Database
-# -----------------------------
+    id:int
+
 STUDENTS = [
-    Student(id=1, name="Prasanna", age=30),
-    Student(id=2, name="John", age=25),
-    Student(id=3, name="Alice", age=21),
-]
+     Student(name="prasanna kumar", age=23, id=1),
+     Student(name="peter nani", age=23, id=2),
+     ]
 
 @strawberry.type
 class Query:
-
     @strawberry.field
-    def student(self) -> Student:
-        return Student(
-            id=1,
-            name="Prasanna",
-            age=30,
-        )
+    def all_students(self)->list[Student]:
+            return STUDENTS
 
-@strawberry.type
-class Mutation:
-
-    @strawberry.mutation
-    def create_student(self, name:str, age:int)->Student:
-        student_4 = Student(
-            id = len(STUDENTS)+1,
-            name = name,
-            age=age
-        )
-        STUDENTS.append(student_4)
-        return student_4
-
-    @strawberry.mutation
-    def update_student(
-        self,
-        id: int,
-        name: str,
-        age: int,
-    ) -> Student:
-
-        for student in STUDENTS:
-
-            if student.id == id:
-                student.name = name
-                student.age = age
-
-                return student
-
-        raise ValueError(f"Student with id={id} not found")
-
-schema = strawberry.Schema(query=Query, mutation=Mutation, config=StrawberryConfig(auto_camel_case=False))
+schema = strawberry.Schema(Query)
