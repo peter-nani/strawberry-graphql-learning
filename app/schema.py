ed5302a01540f4d540a0738b63ccbb18a0df443a
prasanna-kumar <prasanna-kumar@gmail.com>
2026-07-23 23:14:59 +0530

    updated schema V2

--- app/schema.py
+++ app/schema.py
@@ -1,20 +1,71 @@
 import strawberry
+from strawberry.schema.config import StrawberryConfig
 
+
+# -----------------------------
+# Output Type
+# -----------------------------
 @strawberry.type
 class Student:
-    name:str
-    age:int
-    id:int
+    id: int
+    name: str
+    age: int
 
+
+# -----------------------------
+# Input Type
+# -----------------------------
+@strawberry.input
+class StudentInput:
+    id: int
+    name: str
+    age: int
+    department: str
+    email: str
+    phone: str
+
+
+# -----------------------------
+# Fake Database
+# -----------------------------
 STUDENTS = [
-     Student(name="prasanna kumar", age=23, id=1),
-     Student(name="peter nani", age=23, id=2),
-     ]
+    Student(id=1, name="Prasanna", age=30),
+    Student(id=2, name="John", age=25),
+]
 
+
+# -----------------------------
+# Query
+# -----------------------------
 @strawberry.type
 class Query:
-    @strawberry.field
-    def all_students(self)->list[Student]:
-            return STUDENTS
 
-schema = strawberry.Schema(Query)
+    @strawberry.field
+    def students(self) -> list[Student]:
+        return STUDENTS
+
+
+# -----------------------------
+# Mutation
+# -----------------------------
+@strawberry.type
+class Mutation:
+
+    @strawberry.mutation
+    def create_student(
+        self,
+        student: StudentInput,
+    ) -> Student:
+
+        student_data = Student(
+            id=student.id,
+            name=student.name,
+            age=student.age,
+        )
+
+        STUDENTS.append(student_data)
+
+        return student_data
+       
+
+schema = strawberry.Schema(query=Query, mutation=Mutation, config=StrawberryConfig(auto_camel_case=False))
