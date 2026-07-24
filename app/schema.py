aadec009dfa318e774709913eba213728bd54b16
prasanna-kumar <prasanna-kumar@gmail.com>
2026-07-24 22:11:48 +0530

    Shows variables.

--- app/schema.py
+++ app/schema.py
@@ -1,20 +1,44 @@
 import strawberry
+from strawberry.schema.config import StrawberryConfig
 
 @strawberry.type
 class Student:
+    id:int
     name:str
     age:int
-    id:int
 
-STUDENTS = [
-     Student(name="prasanna kumar", age=23, id=1),
-     Student(name="peter nani", age=23, id=2),
-     ]
+@strawberry.input
+class StudentInput:
+    id:int
+    name:str
+    age:int
+
+STUDENTS = []
+
+@strawberry.type
+class Mutation:
+
+    @strawberry.mutation
+    def create_student(
+        self,
+        student:StudentInput,
+    )->Student:
+        new_student = Student(
+            id = student.id,
+            age = student.age,
+            name = student.name,
+        )
+        STUDENTS.append(new_student)
+        return new_student
 
 @strawberry.type
 class Query:
-    @strawberry.field
-    def all_students(self)->list[Student]:
-            return STUDENTS
 
-schema = strawberry.Schema(Query)
+    @strawberry.field
+    def students(self) -> list[Student]:
+        return STUDENTS
+
+
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation, config=StrawberryConfig(auto_camel_case=False))
